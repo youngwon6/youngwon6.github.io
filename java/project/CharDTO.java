18fa46459e7cc1549f8ec2ee636c3bccd9a6e4fb
youngwon6 <143858661+youngwon6@users.noreply.github.com>
2023-12-05 21:04:26 +0900

    Create CharDTO.java

---
+++ java/project/CharDTO.java
@@ -0,0 +1,58 @@
+package chr;
+
+public class CharDTO {
+	String charger_name;
+	String charger_port;
+	String charger_output;
+	String charger_quick;
+	String charger_date;
+	String charger_rental;
+	String rental_name;
+	
+	
+
+	public String getCharger_port() {
+		return charger_port;
+	}
+	public void setCharger_port(String charger_port) {
+		this.charger_port = charger_port;
+	}
+	public String getCharger_output() {
+		return charger_output;
+	}
+	public void setCharger_output(String charger_output) {
+		this.charger_output = charger_output;
+	}
+	public String getCharger_quick() {
+		return charger_quick;
+	}
+	public void setCharger_quick(String charger_quick) {
+		this.charger_quick = charger_quick;
+	}
+	public String getCharger_date() {
+		return charger_date;
+	}
+	public void setCharger_date(String charger_date) {
+		this.charger_date = charger_date;
+	}
+	public String getCharger_name() {
+		return charger_name;
+	}
+	public void setCharger_name(String charger_name) {
+		this.charger_name = charger_name;
+	}
+	public String getCharger_rental() {
+		return charger_rental;
+	}
+	public void setCharger_rental(String charger_rental) {
+		this.charger_rental = charger_rental;
+	}
+	public String getRental_name() {
+		return rental_name;
+	}
+	public void setRental_name(String rental_name) {
+		this.rental_name = rental_name;
+	}
+	
+	
+}
